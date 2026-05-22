59f8d37ed1d673b48d2728b89d2ebefc06d14754
jEuEaEn <jarm1981avrm@gmail.com>
2026-05-21 19:26:02 -0500

    holas

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,26 @@ import Lillian from "./assets/lillianBench.png"
 import Ryley from "./assets/RyleyRobinson.png"
 
 function App() {
+
+    const characters =[
+      {
+        
+        imagen: Lillian,
+        nombre: "Lillian Bench",
+        rol: "Xenobiologa"
+      },
+      {
+        
+        imagen: Ryley,
+        nombre: "Ryley Robinson",
+        rol: "Jefe de mantenimiento de Sistemas No Esenciales"
+      }
+    ]
+
   return (<>
-        <Saludo saludo = "Jole"/>
-        <div className="tarjetas-container">
-          <Tarjeta imagen={Lillian} nombre={"Lillian Bench"} rol={"Xenobiologa"}/>
-          <Tarjeta imagen={Ryley} nombre={"Ryley Robinson"} rol={"Jefe de mantenimiento de Sistemas No Esenciales"}/>
-          
-        </div>
+        {characters.map((personaje) =>{
+           return <Tarjeta imagen={personaje.imagen} nombre={personaje.nombre} rol={personaje.rol} />
+        })}
     </>
   )
 }
